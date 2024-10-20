ad688a37a1404529f89a7cc48878b4752335e479
Özgür Yaman <81265246+ozguaman@users.noreply.github.com>
2024-10-20 23:27:24 +0300

    Added features

--- java-shop-system-console-app/app.java
+++ java-shop-system-console-app/app.java
@@ -2,55 +2,90 @@ import java.util.Scanner;
 
 class Project {
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("*-- MARKET PRODUCT SYSTEM --*\n");
-		
-		String userName = "admin";
-		int password = 123;
+		boolean run = true;
 
-		System.out.println("Username and password are included in the source code.\n");
-		System.out.print("Username: ");
-		String enteredUserName = keyboard.next();
-		System.out.print("Password: ");
-		int enteredPass = keyboard.nextInt();
+		while(run) {
+			Scanner keyboard = new Scanner(System.in);
+			System.out.println("*-- PRODUCT SYSTEM OF MARKET --*\n");
+            
+			String userName = "admin";
+			int password = 123;
 
-		if(enteredUserName.equals(userName)  && enteredPass == password) {
-			System.out.println("Login successful! \n\n");
-			System.out.print("If you want to enter a product into the system, type '/login'.:$ ");
-			String komut = keyboard.next();
+			System.out.println("[1] Open the System");
+			System.out.println("[2] Quit");
+			System.out.print("(1,2): $ ");
+			int choice = keyboard.nextInt();
 
-			if(komut.equals("/login")) {
-				System.out.print("How many products do you want to add? ");
-				int enteredProduct = keyboard.nextInt();
-				double totalPrice = 0;
-				String productList = "\n";
+			switch(choice) {
+				case 1:
+					System.out.println("\nUsername and password are included in the source code.\n");
+					System.out.print("Username: ");
+					String enteredUserName = keyboard.next();
+					System.out.print("Password: ");
+					int enteredPass = keyboard.nextInt();		
 
-				for(int i = 1; i<= enteredProduct; i++) {
-					System.out.print("Product name: ");
-					String name = keyboard.next();
-					System.out.print("Number of products: ");
-					int number = keyboard.nextInt();
-					System.out.print("Product price(ex: 2, 6,5): "); 
-					double price = keyboard.nextDouble();
+					if(enteredUserName.equals(userName)  && enteredPass == password) {
+						System.out.println("Login successful! \n\n");
+						System.out.print("If you want to enter a product into the system, type '/login'.:$ ");
+						String komut = keyboard.next();
 
-					totalPrice = totalPrice + price*number;
-					productList = productList + "[" + i + "] " + name + " | " + "#" + number + " | " + price + "$ | " + "Total: " + number*price + "$\n";
+						boolean run2 = true;
 
-					System.out.println("Added Product: " + name + ", #" + number + ", " + price + "$\n");
+						while(run2) {
+							if(komut.equals("/login")) {
+								System.out.print("How many products do you want to add? ");
+								int enteredProduct = keyboard.nextInt();
+								double totalPrice = 0;
+								String productList = "\n";
 
-					if(i == enteredProduct) {
-						System.out.println("-$- PRODUCTS -$-");
-						System.out.println("----------------------------");
-						System.out.println(productList);
-						System.out.println("Total Price: " + totalPrice + "$\n");
-						System.out.println("----------------------------");
+								for(int i = 1; i<= enteredProduct; i++) {
+									System.out.print("Product name: ");
+									String name = keyboard.next();
+									System.out.print("Number of products: ");
+									int number = keyboard.nextInt();
+									System.out.print("Product price(for example; 2, 6,5): "); 
+									double price = keyboard.nextDouble();
+
+									totalPrice = totalPrice + price*number;
+									productList =  productList + "| " + "[" + i + "] " + name + " | " + "#" + number + " | " + price + "$ | " + "Total: " + number*price + "$ |\n" + "---------------------------------------------------------\n";
+
+									System.out.println("Added Product: " + name + ", #" + number + ", " + price + "$\n");
+
+									if(i == enteredProduct) {
+										System.out.println("-$- PRODUCTS -$-");
+										System.out.println("----------------------------\n");
+									    System.out.print("---------------------------------------------------------");
+										System.out.println(productList);
+										System.out.println("Total Price: " + totalPrice + "$\n");
+										System.out.println("----------------------------");
+
+
+										System.out.println("[1] Change Products");
+										System.out.println("[2] Back to Menu	");
+										System.out.print("(1,2): $ ");
+										int choice2 = keyboard.nextInt();
+
+										switch(choice2) {
+											case 1:
+												break;
+											case 2:
+												run2 = false;
+										}
+									}
+								}
+							} else {
+								System.out.println("You entered the command incorrectly. Please try again.");
+							}
+						}	
+					} else {
+						System.out.println("You entered your username or password incorrectly.");
+						continue;
 					}
-				}
-			} else {
-				System.out.println("You entered the command incorrectly. Please try again.");
+					break;
+				case 2:
+					run = false;
+					break;
 			}
-		} else {
-			System.out.println("You entered your username or password incorrectly.");
 		}
 	}
 }
